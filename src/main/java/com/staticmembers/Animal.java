9105a26f6f95140ae991530c6500f7d5c5cb0031
Yared Tseghu <yaredtseghu@Yareds-MacBook-Pro-3.local>
2016-04-09 19:53:32 +0100

    Removed unnecessary code

--- src/main/java/com/staticmembers/Animal.java
+++ src/main/java/com/staticmembers/Animal.java
@@ -1,7 +1,19 @@
 package com.staticmembers;
 
-/**
- * Created by yaredtseghu on 09/04/2016.
- */
 public class Animal {
+
+    public static String test = "Test";
+
+    public static void testApp(){
+        System.out.println( "This is from static method ");
+    }
+
+    public static void main( String... strings){
+        System.out.println( Animal.test             );
+        System.out.println( new Animal().test       );
+        Animal.testApp();
+        new Animal().testApp();
+
+        System.getProperties().list( Animal );
+    }
 }
